8389aea9d08ead2188ca109a82441e63966a3e52
LeaYeh <smile2140@gmail.com>
2024-05-29 21:43:42 +0000

    chore: Record for how to handle debug folder

--- build/source_files_miniRT.mk
+++ build/source_files_miniRT.mk
@@ -6,7 +6,7 @@
 #    By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/03/19 16:15:33 by ldulling          #+#    #+#              #
-#    Updated: 2024/05/28 12:06:12 by lyeh             ###   ########.fr        #
+#    Updated: 2024/05/29 19:04:19 by lyeh             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,6 +15,7 @@
 
 #TODO Temporary solution to get all source files
 SRC				:=	$(patsubst source/%,%,$(wildcard source/*.c source/**/*.c))
+# SRC				+=	$(addprefix ../,$(wildcard debug/*.c debug/**/*.c))
 
 # Main:
 # DIR		:=	./
