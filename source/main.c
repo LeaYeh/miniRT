6b46a7b935328c4938dcb4c3941661450ce7cf6d
LeaYeh <smile2140@gmail.com>
2024-06-14 20:33:55 +0000

    feat: Add test case for camera

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 16:00:13 by lyeh              #+#    #+#             */
-/*   Updated: 2024/06/01 11:15:04 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/06/14 20:44:20 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	main(int argc, char **argv)
 		return (ERR_ARGC);
 	}
 	if (!init_minirt(&minirt, argv[1]))
-		return (ERR_INIT);
+		return (free_minirt(&minirt), ERR_INIT);
 	print_scene(minirt.scene);
 	free_minirt(&minirt);
 	return (SUCCESS);
--- source/minirt.c
+++ source/minirt.c
@@ -6,17 +6,21 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 19:36:14 by lyeh              #+#    #+#             */
-/*   Updated: 2024/06/01 11:32:50 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/06/14 19:53:14 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+#include "debug.h"
 
 bool	init_minirt(t_minirt *minirt, char *filename)
 {
 	minirt->scene = read_scene(filename);
 	if (!minirt->scene)
 		return (false);
+	if (!init_camera(&minirt->scene->camera))
+		return (false);
+	print_camera_detail(minirt->scene->camera);
 	return (true);
 }
 
