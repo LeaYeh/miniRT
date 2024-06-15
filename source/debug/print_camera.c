a698a9e83465606a9308838875c12003a424716b
LeaYeh <smile2140@gmail.com>
2024-06-15 13:37:58 +0000

    fix: Add ray list size info into debug message

--- source/debug/print_camera.c
+++ source/debug/print_camera.c
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 19:49:52 by lyeh              #+#    #+#             */
-/*   Updated: 2024/06/14 19:52:03 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/06/15 13:34:11 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,5 @@ void    print_camera_detail(t_camera camera)
     print_vector(camera.pixel.delta_u);
     printf("\t\tDelta V: ");
     print_vector(camera.pixel.delta_v);
+    printf("\tRay list size: %d\n", ft_lstsize(camera.ray_list));
 }
