f685d46fe04c22d4b68d26d9fe056bf931a8f1d9
LeaYeh <smile2140@gmail.com>
2024-06-16 12:25:46 +0000

    feat: Init color map for final output structure for mlx

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:18:37 by lyeh              #+#    #+#             */
-/*   Updated: 2024/06/15 14:47:00 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/06/16 12:20:59 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 typedef struct s_minirt
 {
 	t_scene	*scene;
+	t_vec3	*color_map;
 }	t_minirt;
 
 bool	init_minirt(t_minirt *minirt, char *filename);
--- source/minirt.c
+++ source/minirt.c
@@ -6,22 +6,41 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 19:36:14 by lyeh              #+#    #+#             */
-/*   Updated: 2024/06/15 14:47:21 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/06/16 12:24:45 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #include "debug.h"
 
+static bool	init_color_map(t_minirt *minirt);
+
 bool	init_minirt(t_minirt *minirt, char *filename)
 {
 	minirt->scene = read_scene(filename);
 	if (!minirt->scene)
 		return (false);
-	if (!init_camera(&minirt->scene->camera))
+	if (!init_camera(&minirt->scene->camera) || !init_color_map(minirt))
 		return (false);
 	print_camera_detail(minirt->scene->camera);
 	print_pixel_grid(minirt->scene->camera.pixel);
+	return (true);
+}
+
+bool	init_color_map(t_minirt *minirt)
+{
+	int	i;
+
+	minirt->color_map = (t_vec3 *)malloc(
+			sizeof(t_vec3) * WINDOW_WIDTH * WINDOW_HEIGHT);
+	if (!minirt->color_map)
+		return (false);
+	i = 0;
+	while (i < WINDOW_WIDTH * WINDOW_HEIGHT)
+	{
+		minirt->color_map[i] = (t_vec3){0, 0, 0};
+		i++;
+	}
 	return (true);
 }
 
