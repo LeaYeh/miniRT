865f19b98f2e1e2d7e2e3a70f440a6a4f5568e3c
LeaYeh <smile2140@gmail.com>
2024-06-16 12:37:41 +0000

    fix: Init with missing .ops

--- source/minirt.c
+++ source/minirt.c
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 19:36:14 by lyeh              #+#    #+#             */
-/*   Updated: 2024/06/16 12:24:45 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/06/16 12:37:13 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ bool	init_color_map(t_minirt *minirt)
 	i = 0;
 	while (i < WINDOW_WIDTH * WINDOW_HEIGHT)
 	{
-		minirt->color_map[i] = (t_vec3){0, 0, 0};
+		minirt->color_map[i] = (t_vec3){0, 0, 0, NULL};
 		i++;
 	}
 	return (true);
