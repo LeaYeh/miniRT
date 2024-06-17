853b8a6957a20cfd9f74477b8dbd444b7a94ea64
LeaYeh <smile2140@gmail.com>
2024-06-17 15:18:42 +0000

    feat: Add unit test cases for hit plane

---
+++ tests/test_hit_plane.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_hit_plane.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/16 13:27:23 by lyeh              #+#    #+#             */
+/*   Updated: 2024/06/17 15:17:00 by lyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "unit_tests.h"
+#include "debug.h"
+
+Test(hit, hit_plane_under)
+{
+	const t_vec3	vec3 = {.ops = init_ops()};
+	t_ray			ray;
+	t_obj			plane;
+	t_hit_record	rec;
+
+	ray = (t_ray){
+		.origin = (t_vec3){.x = 0, .y = -1.0, .z = 0},
+		.direction = (t_vec3){.x = 0, .y = 1, .z = 0}};
+	plane = (t_obj){
+		.position = (t_vec3){.x = 0, .y = 0, .z = 0},
+		.norm = (t_vec3){.x = 0, .y = 1, .z = 0}};
+	cr_assert_eq(hit_plane(vec3, &ray, &plane, &rec), true, "hit_plane() detection incorrect");
+	cr_assert_eq(rec.t, 1.0, "hit_plane() t value incorrect");
+	cr_assert_eq(rec.front_face, false, "hit_plane() front_face value incorrect");
+	cr_assert_eq(rec.point.x, 0.0, "hit_plane() point.x value incorrect");
+	cr_assert_eq(rec.point.y, 0.0, "hit_plane() point.y value incorrect");
+	cr_assert_eq(rec.point.z, 0.0, "hit_plane() point.z value incorrect");
+	cr_assert_eq(rec.norm.x, 0.0, "hit_plane() norm.x value incorrect");
+	cr_assert_eq(rec.norm.y, -1.0, "hit_plane() norm.y value incorrect");
+	cr_assert_eq(rec.norm.z, 0.0, "hit_plane() norm.z value incorrect");
+}
+
+Test(hit, hit_plane_above)
+{
+	const t_vec3	vec3 = {.ops = init_ops()};
+	t_ray			ray;
+	t_obj			plane;
+	t_hit_record	rec;
+
+	ray = (t_ray){
+		.origin = (t_vec3){.x = 0, .y = 2.0, .z = 0},
+		.direction = (t_vec3){.x = 0, .y = -1, .z = 0}};
+	plane = (t_obj){
+		.position = (t_vec3){.x = 0, .y = 0, .z = 0},
+		.norm = (t_vec3){.x = 0, .y = 1, .z = 0}};
+	cr_assert_eq(hit_plane(vec3, &ray, &plane, &rec), true, "hit_plane() detection incorrect");
+	cr_assert_eq(rec.t, 2.0, "hit_plane() t value incorrect");
+	cr_assert_eq(rec.front_face, true, "hit_plane() front_face value incorrect");
+	cr_assert_eq(rec.point.x, 0.0, "hit_plane() point.x value incorrect");
+	cr_assert_eq(rec.point.y, 0.0, "hit_plane() point.y value incorrect");
+	cr_assert_eq(rec.point.z, 0.0, "hit_plane() point.z value incorrect");
+	cr_assert_eq(rec.norm.x, 0.0, "hit_plane() norm.x value incorrect");
+	cr_assert_eq(rec.norm.y, 1.0, "hit_plane() norm.y value incorrect");
+	cr_assert_eq(rec.norm.z, 0.0, "hit_plane() norm.z value incorrect");
+}
+
+Test(hit, hit_plane_miss)
+{
+	const t_vec3	vec3 = {.ops = init_ops()};
+	t_ray			ray;
+	t_obj			plane;
+	t_hit_record	rec;
+
+	ray = (t_ray){
+		.origin = (t_vec3){.x = 0, .y = 1.0, .z = 0},
+		.direction = (t_vec3){.x = 0, .y = 1, .z = 0}};
+	plane = (t_obj){
+		.position = (t_vec3){.x = 0, .y = 0, .z = 0},
+		.norm = (t_vec3){.x = 0, .y = 1, .z = 0}};
+	cr_assert_eq(hit_plane(vec3, &ray, &plane, &rec), false, "hit_plane() detection incorrect");
+}
--- tests/unit_tests.h
+++ tests/unit_tests.h
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 15:35:54 by lyeh              #+#    #+#             */
-/*   Updated: 2024/06/02 21:01:10 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/06/16 13:29:41 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,8 @@
 # include "vec3_private.h"
 # include "camera_private.h"
 # include "reader_private.h"
+# include "ray_private.h"
+# include "hit_private.h"
+# include "object.h"
 
 #endif
