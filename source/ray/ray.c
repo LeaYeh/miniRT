01a2a771f9e856d619ab169f78dfee23dac57c6d
LeaYeh <smile2140@gmail.com>
2024-06-22 20:02:56 +0000

    fix: Change the light_dir definition to fix the specular

--- source/ray/ray.c
+++ source/ray/ray.c
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 14:57:25 by lyeh              #+#    #+#             */
-/*   Updated: 2024/06/21 13:41:00 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/06/22 19:07:19 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,9 @@ t_vec3	reflect_direction(t_vec3 shoot_dir, t_vec3 p_norm)
 	const t_vec3	vec3 = (t_vec3){.ops = init_ops()};
 	t_vec3			reflect;
 
-	reflect = vec3.ops->sub(
-			shoot_dir,
-			vec3.ops->mul(p_norm, 2 * vec3.ops->dot(shoot_dir, p_norm)));
+	reflect = vec3.ops->normalize(
+			vec3.ops->sub(
+				shoot_dir,
+				vec3.ops->mul(p_norm, 2 * vec3.ops->dot(shoot_dir, p_norm))));
 	return (reflect);
 }
