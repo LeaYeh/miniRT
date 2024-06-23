f2707fc8da4632a01593eab302e73f3e2a9e20d7
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-06-23 15:53:28 +0200

    feat: Close window by Escape key

--- source/mlx/events.c
+++ source/mlx/events.c
@@ -7,8 +7,17 @@ int	clean_and_exit(t_minirt *minirt)
 	exit (0);
 }
 
+int	handle_key_event(int keysym, t_minirt *minirt)
+{
+	if (keysym == XK_Escape)
+		clean_and_exit(minirt);
+	return (0);
+}
+
 void	setup_event_hooks(t_minirt *minirt)
 {
 	mlx_hook(minirt->mlx.win_ptr,
 		DestroyNotify, NoEventMask, clean_and_exit, minirt);
+	mlx_hook(minirt->mlx.win_ptr,
+		KeyPress, KeyPressMask, handle_key_event, minirt);
 }
