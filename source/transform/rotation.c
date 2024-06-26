fbafa2cb1e9dd0510b944b48480fd39be7a23eec
Lea Yeh <lea.yeh.ml@gmail.com>
2024-06-26 10:16:37 +0200

    refact: Change the transform functions API

--- source/transform/rotation.c
+++ source/transform/rotation.c
@@ -3,55 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   rotation.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 22:17:52 by lyeh              #+#    #+#             */
-/*   Updated: 2024/06/25 20:08:14 by ldulling         ###   ########.fr       */
+/*   Updated: 2024/06/26 10:11:05 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "transform.h"
 
-static void	rotate_x(t_vec3 *v, double angle);
-static void	rotate_y(t_vec3 *v, double angle);
-static void	rotate_z(t_vec3 *v, double angle);
+static t_vec3	rotate_x(t_vec3 v, double angle);
+static t_vec3	rotate_y(t_vec3 v, double angle);
+static t_vec3	rotate_z(t_vec3 v, double angle);
 
-void	rotate(t_vec3 *v, t_vec3 rotation)
+t_vec3	rotate(t_vec3 v, t_vec3 rotation)
 {
-	rotate_x(v, rotation.x);
-	rotate_y(v, rotation.y);
-	rotate_z(v, rotation.z);
+	t_vec3	result;
+
+	result = rotate_x(v, rotation.x);
+	result = rotate_y(result, rotation.y);
+	result = rotate_z(result, rotation.z);
+	return (result);
 }
 
-static void	rotate_x(t_vec3 *v, double angle)
+static t_vec3	rotate_x(t_vec3 v, double angle)
 {
-	double	y;
-	double	z;
+	const double	rad = angle * M_PI / 180.0;
+	double			y;
+	double			z;
 
-	y = v->y * cos(angle) - v->z * sin(angle);
-	z = v->y * sin(angle) + v->z * cos(angle);
-	v->y = y;
-	v->z = z;
+	y = v.y * cos(rad) - v.z * sin(rad);
+	z = v.y * sin(rad) + v.z * cos(rad);
+	return (vector(v.x, y, z));
 }
 
-static void	rotate_y(t_vec3 *v, double angle)
+static t_vec3	rotate_y(t_vec3 v, double angle)
 {
+	const double	rad = angle * M_PI / 180.0;
+	double			x;
+	double			z;
+
+	x = v.x * cos(rad) + v.z * sin(rad);
+	z = -v.x * sin(rad) + v.z * cos(rad);
+	return (vector(x, v.y, z));
+}
+
+static t_vec3	rotate_z(t_vec3 v, double angle)
+{
+	const double	rad = angle * M_PI / 180.0;
 	double	x;
-	double	z;
-
-	x = v->x * cos(angle) + v->z * sin(angle);
-	z = -v->x * sin(angle) + v->z * cos(angle);
-	v->x = x;
-	v->z = z;
-}
-
-static void	rotate_z(t_vec3 *v, double angle)
-{
-	double	x;
 	double	y;
 
-	x = v->x * cos(angle) - v->y * sin(angle);
-	y = v->x * sin(angle) + v->y * cos(angle);
-	v->x = x;
-	v->y = y;
+	x = v.x * cos(rad) - v.y * sin(rad);
+	y = v.x * sin(rad) + v.y * cos(rad);
+	return (vector(x, y, v.z));
 }
