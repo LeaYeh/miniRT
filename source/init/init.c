e949eefa3b9d2bb48c340168147edc3f8c1a5a81
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-06-30 19:20:42 +0200

    style: Split up minirt.c into init.c and clean.c

--- source/init/init.c
+++ source/init/init.c
@@ -1,18 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minirt.c                                           :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 19:36:14 by lyeh              #+#    #+#             */
-/*   Updated: 2024/06/29 23:08:24 by ldulling         ###   ########.fr       */
+/*   Updated: 2024/06/30 19:16:02 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-#include "render.h"
-#include "mlx_utils.h"
 
 static bool		init_ray_pool(t_ray **ray_pool, t_camera *camera);
 static t_ray	create_ray_from_pixel_grid(t_camera *camera, int row, int col);
@@ -28,38 +26,6 @@ bool	init_minirt(t_minirt *minirt, char *filename)
 		return (false);
 	minirt->stage = CAMERA_CHANGE;
 	return (true);
-}
-
-void	free_ray_pool(t_ray **ray_pool, t_pixel_grid *pixel)
-{
-	int	i;
-	int	j;
-
-	if (!pixel)
-		return ;
-	i = 0;
-	while (i < pixel->row_size)
-	{
-		j = 0;
-		while (j < pixel->col_size)
-		{
-			ft_lstclear(
-				&(*ray_pool)[i * pixel->col_size + j].hit_record_list, free);
-			j++;
-		}
-		i++;
-	}
-	ft_free_and_null((void **)ray_pool);
-}
-
-void	free_minirt(t_minirt *minirt)
-{
-	free_mlx(&minirt->mlx);
-	if (minirt->scene)
-	{
-		free_ray_pool(&minirt->ray_pool, &minirt->scene->camera.pixel);
-		free_scene(&minirt->scene);
-	}
 }
 
 void	reset_ray_pool(t_ray *ray_pool, t_camera *camera)
